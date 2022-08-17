f270ba39ea6d69413351746c0f368c21d42e6f4f
kimkj38 <kimkj389@gmail.com>
2022-08-17 10:31:40 +0900

    Generate point cloud of subset

--- generatePC.py
+++ generatePC.py
@@ -6,22 +6,6 @@ import os
 # scan id
 scan_id = "4acaebc8-6c10-2a2a-8525-fe9c4b7f4b25"
 
-# get rgb from hex code
-def hex2rgb(hex):
-    hex = hex.lstrip('#')
-    r,g,b = tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
-    return r,g,b
-
-# class dictionary
-word2idx = {} # key: object name, value: object id
-index = 0
-file = open("classes.txt", 'r')
-category = file.readline()[:-1]
-while category:
-    word2idx[category] = index
-    category = file.readline()[:-1]
-    index += 1
-
 # subset json
 with open('subset.json') as f:
     subset_file = json.load(f)["scans"]
@@ -30,27 +14,52 @@ with open('subset.json') as f:
 with open('original.json') as f:
     original_file = json.load(f)["scans"]
 
+# get rgb from hex code
+def hex2rgb(hex):
+    hex = hex.lstrip('#')
+    r,g,b = tuple(int(hex[i:i+2], 16) for i in (0, 2, 4))
+    return r,g,b
+
+# class dictionary
+def get_word2idx():
+    word2idx = {} # key: object name, value: object id
+    index = 0
+    file = open("classes.txt", 'r')
+    category = file.readline()[:-1]
+    while category:
+        word2idx[category] = index
+        category = file.readline()[:-1]
+        index += 1
+
+    return word2idx
+
 # point cloud
-with open('{}/mesh.refined.v2.obj'.format(scan_id)) as f:
-    point_cloud = []
-    while 1:
-        line = f.readline()
-        if not line:
-            break
-        strs = line.split(" ")
-        if strs[0] == "v":
-            point_cloud.append((float(strs[1]), float(strs[2]), float(strs[3])))
-    point_cloud = np.array(point_cloud)
+def get_pc(scan_id):
+    with open('{}/mesh.refined.v2.obj'.format(scan_id)) as f:
+        point_cloud = []
+        while 1:
+            line = f.readline()
+            if not line:
+                break
+            strs = line.split(" ")
+            if strs[0] == "v":
+                point_cloud.append((float(strs[1]), float(strs[2]), float(strs[3])))
+        point_cloud = np.array(point_cloud)
+
+    return point_cloud
 
 
 # segment & point cloud correspondence
-segments = {}  # key:segment id, value: points belong to this segment
-with open("{}/mesh.refined.0.010000.segs.v2.json".format(scan_id), 'r') as f:
-    seg_indices = json.load(f)["segIndices"]
-    for index, i in enumerate(seg_indices):
-        if i not in segments:
-            segments[i] = []
-        segments[i].append(point_cloud[index])
+def get_seg2pc(scan_id, point_cloud):
+    seg2pc = {}  # key:segment id, value: points belong to this segment
+    with open("{}/mesh.refined.0.010000.segs.v2.json".format(scan_id), 'r') as f:
+        seg_indices = json.load(f)["segIndices"]
+        for index, i in enumerate(seg_indices):
+            if i not in seg2pc:
+                seg2pc[i] = []
+            seg2pc[i].append(point_cloud[index])
+
+    return seg2pc
 
 # filter the object which does not belong to this split
 # obj_id_list = []
@@ -58,37 +67,56 @@ with open("{}/mesh.refined.0.010000.segs.v2.json".format(scan_id), 'r') as f:
 #     obj_id_list.append(int(obj_id))
 
 # seg groups
-with open("{}/semseg.v2.json".format(scan_id), 'r') as f:
-    obj2pc = {} # key: object id, value: point cloud
-    seg2obj = {} # key: segment id, value: object id
-    seg_groups = json.load(f)["segGroups"]
-    for object in seg_groups:
-        object_id = object["id"]
-        # if object_id not in obj_id_list:
-        #     continue
-        # if object["label"] not in  word2idx:
-        #     continue
+def get_obj2pc(scan_id, seg2pc):
+    with open("{}/semseg.v2.json".format(scan_id), 'r') as f:
+        obj2pc = {} # key: object id, value: point cloud
+        seg2obj = {} # key: segment id, value: object id
+        seg_groups = json.load(f)["segGroups"]
+        for object in seg_groups:
+            object_id = object["id"]
+            # if object_id not in obj_id_list:
+            #     continue
+            # if object["label"] not in  word2idx:
+            #     continue
 
-        seg_ids = object["segments"]
-        obj2pc[object_id] = []
-        for seg_id in seg_ids:
-            seg2obj[seg_id] = object_id
-            for pc in segments[seg_id]:
-                #print(np.concatenate((obj2pc[object_id], pc.reshape(1,-1)), axis=0))
-                obj2pc[object_id] = pc.reshape(1, -1) if len(obj2pc[object_id]) == 0 else np.concatenate((obj2pc[object_id], pc.reshape(1, -1)), axis=0)
-
+            seg_ids = object["segments"]
+            obj2pc[object_id] = []
+            for seg_id in seg_ids:
+                seg2obj[seg_id] = object_id
+                for pc in seg2pc[seg_id]:
+                    #print(np.concatenate((obj2pc[object_id], pc.reshape(1,-1)), axis=0))
+                    obj2pc[object_id] = pc.reshape(1, -1) if len(obj2pc[object_id]) == 0 else np.concatenate((obj2pc[object_id], pc.reshape(1, -1)), axis=0)
+    
+    return obj2pc
 
 # hex code dictionary
-for i, scan in enumerate(original_file):
-    if scan["scan"] == scan_id:
-        hex_dict = {} # key: object id, value: hex code
-        global_dict = {} # key: object id, value: global id
-        objects = original_file[i]["objects"]
-        for object in objects:
-            hex_dict[object["id"]] = object["ply_color"]
-            global_dict[object["id"]] = object["global_id"]
+def get_obj2global(scan_id):
+    for i, scan in enumerate(original_file):
+        if scan["scan"] == scan_id:
+            obj2global = {} # key: object id, value: global id
+            objects = original_file[i]["objects"]
+            for object in objects:
+                obj2global[object["id"]] = object["global_id"]
+
+    return obj2global
+
+def get_obj2hex(scan_id):
+    for i, scan in enumerate(original_file):
+        if scan["scan"] == scan_id:
+            obj2hex = {} # key: object id, value: hex code
+            objects = original_file[i]["objects"]
+            for object in objects:
+                obj2hex[object["id"]] = object["ply_color"]
+
+    return obj2hex
 
 
+
+point_cloud = get_pc(scan_id)
+seg2pc = get_seg2pc(scan_id, point_cloud)
+obj2pc = get_obj2pc(scan_id, seg2pc)
+obj2global = get_obj2global(scan_id)
+obj2hex = get_obj2hex(scan_id)
 
 for i, scan in enumerate(subset_file):
     if scan["scan"] == scan_id:
@@ -101,8 +129,8 @@ for i, scan in enumerate(subset_file):
         vertex = []
         for obj_id in object_ids:
             pc = obj2pc[int(obj_id)]
-            global_id = int(global_dict[obj_id])
-            r, g, b = hex2rgb(hex_dict[obj_id])
+            global_id = int(obj2global[obj_id])
+            r, g, b = hex2rgb(obj2hex[obj_id])
 
             for x,y,z in pc:
                 if len(vertex) == 0:
